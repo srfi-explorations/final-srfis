919367b76206bfb5973e32701917108a21742ba6
Mike Sperber <sperber@srfi.schemers.org>
2002-06-04 16:16:16 +0000

    Revision from Sebastian Egner.

--- check.scm
+++ check.scm
@@ -1,7 +1,7 @@
 ; CONFIDENCE TEST FOR IMPLEMENTATION OF SRFI-26
 ; =============================================
 ;
-; Sebastian.Egner@philips.com, 28-Feb-2002.
+; Sebastian.Egner@philips.com, 3-Jun-2002.
 ;
 ; This file checks a few assertions about the implementation.
 ; If you run it and no error message is issued, the implementation
@@ -28,7 +28,8 @@
 (define (check-all)
   (for-each 
    check
-   '((equal? ((cut list)) '())
+   '( ; cuts
+     (equal? ((cut list)) '())
      (equal? ((cut list <...>)) '())
      (equal? ((cut list 1)) '(1))
      (equal? ((cut list <>) 1) '(1))
@@ -39,7 +40,31 @@
      (equal? ((cut list 1 <...>) 2 3 4) '(1 2 3 4))
      (equal? ((cut list 1 <> 3 <>) 2 4) '(1 2 3 4))
      (equal? ((cut list 1 <> 3 <...>) 2 4 5 6) '(1 2 3 4 5 6))
-     (equal? (let* ((x 'wrong) (y (cut list x))) (set! x 'ok) (y)) '(ok)))))
+     (equal? (let* ((x 'wrong) (y (cut list x))) (set! x 'ok) (y)) '(ok))
+     (equal? 
+      (let ((a 0))
+	(map (cut + (begin (set! a (+ a 1)) a) <>)
+	     '(1 2))
+	a)
+      2)
+      ; cutes
+     (equal? ((cute list)) '())
+     (equal? ((cute list <...>)) '())
+     (equal? ((cute list 1)) '(1))
+     (equal? ((cute list <>) 1) '(1))
+     (equal? ((cute list <...>) 1) '(1))
+     (equal? ((cute list 1 2)) '(1 2))
+     (equal? ((cute list 1 <>) 2) '(1 2))
+     (equal? ((cute list 1 <...>) 2) '(1 2))
+     (equal? ((cute list 1 <...>) 2 3 4) '(1 2 3 4))
+     (equal? ((cute list 1 <> 3 <>) 2 4) '(1 2 3 4))
+     (equal? ((cute list 1 <> 3 <...>) 2 4 5 6) '(1 2 3 4 5 6))
+     (equal? 
+      (let ((a 0))
+	(map (cute + (begin (set! a (+ a 1)) a) <>)
+	     '(1 2))
+	a)
+      1))))
 
 ; run the checks when loading
 (check-all)
--- cut.scm
+++ cut.scm
@@ -1,11 +1,15 @@
 ; REFERENCE IMPLEMENTATION FOR SRFI-26 "CUT"
 ; ==========================================
 ;
-; Sebastian.Egner@philips.com, 27-Feb-2002.
+; Sebastian.Egner@philips.com, 4-Jun-2002.
 ;
 ; The code to handle the variable argument case was originally
 ; proposed by Michael Sperber and has been adapted to the new
-; syntax of the macro using an explicit rest-slot symbol.
+; syntax of the macro using an explicit rest-slot symbol. The
+; code to evaluate the non-slots for cute has been proposed by
+; Dale Jordan. The code to allow a slot for the procedure position
+; and to process the macro using an internal macro is based on 
+; a suggestion by Al Petrofsky.
 ;
 ; compliance:
 ;   Scheme R5RS (including macros).
@@ -17,22 +21,42 @@
 ;   SE,  6-Feb-2002: initial version as 'curry' with ". <>" notation
 ;   SE, 14-Feb-2002: revised for <...>
 ;   SE, 27-Feb-2002: revised for 'cut'
+;   SE, 03-Jun-2002: revised for proc-slot, cute
+;   SE, 04-Jun-2002: rewritten with internal transformer (no "loop" pattern)
+
+; (srfi-26-internal mode params args . cs)
+;   transformer used internally
+;     mode   : either "cut" or "cute"
+;     params : list of parameters of the specialized procedure
+;     args   : procedure being specialized, followed by its arguments
+;     cs     : consts-or-slots, the qualifiers of the macro
+
+(define-syntax srfi-26-internal
+  (syntax-rules (<> <...>)
+
+    ; construct variable- or fixed-arity procedure
+    ((srfi-26-internal mode (param ...) (proc arg ...) <...>)
+     (lambda (param ... . rest-slot) (apply proc arg ... rest-slot)))
+    ((srfi-26-internal mode (param ...) (proc arg ...))
+     (lambda (param ...) (proc arg ...)))
+
+    ; loop over cs
+    ((srfi-26-internal mode (param ...) (proc-arg ...) <> . cs)
+     (srfi-26-internal mode (param ... slot) (proc-arg ... slot) . cs))
+    ((srfi-26-internal "cute" (param ...) (proc-arg ...) (c-op . c-args) . cs)
+     (let ((const (c-op . c-args)))
+       (srfi-26-internal "cute" (param ...) (proc-arg ... const) . cs)))
+    ((srfi-26-internal mode (param ...) (proc-arg ...) const . cs)
+     (srfi-26-internal mode (param ...) (proc-arg ... const) . cs))))
+
+; exported syntax
 
 (define-syntax cut
   (syntax-rules (<> <...>)
+    ((cut . consts-or-slots)
+     (srfi-26-internal "cut" () () . consts-or-slots))))
 
-    ; construct variable or fixed arity procedure
-    ((cut "loop" (params ...) proc (args ...) <...>)
-     (lambda (params ... . rest-slot) (apply proc args ... rest-slot)))
-    ((cut "loop" (params ...) proc (args ...))
-     (lambda (params ...) (proc args ...)))
-				
-    ; loop over <const-or-slot>*
-    ((cut "loop" (param ...) proc (arg ...) <> . consts-or-slots)
-     (cut "loop" (param ... slot) proc (arg ... slot) . consts-or-slots))
-    ((cut "loop" (param ...) proc (arg ...) const . consts-or-slots)
-     (cut "loop" (param ...) proc (arg ... const) . consts-or-slots))
-
-    ; specified syntax
-    ((cut proc . consts-or-slots)
-     (cut "loop" () proc () . consts-or-slots))))
+(define-syntax cute
+  (syntax-rules (<> <...>)
+    ((cute . consts-or-slots)
+     (srfi-26-internal "cute" () () . consts-or-slots))))
