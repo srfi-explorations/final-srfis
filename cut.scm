96a00db1485ccd769277f6051ff7e749c835c143
Mike Sperber <sperber@srfi.schemers.org>
2002-06-06 11:11:39 +0000

    Revision from Sebastian Egner.

--- cut.scm
+++ cut.scm
@@ -1,7 +1,8 @@
 ; REFERENCE IMPLEMENTATION FOR SRFI-26 "CUT"
 ; ==========================================
 ;
-; Sebastian.Egner@philips.com, 4-Jun-2002.
+; Sebastian.Egner@philips.com, 5-Jun-2002.
+; adapted from the posting by Al Petrofsky <al@petrofsky.org>
 ;
 ; The code to handle the variable argument case was originally
 ; proposed by Michael Sperber and has been adapted to the new
@@ -9,7 +10,9 @@
 ; code to evaluate the non-slots for cute has been proposed by
 ; Dale Jordan. The code to allow a slot for the procedure position
 ; and to process the macro using an internal macro is based on 
-; a suggestion by Al Petrofsky.
+; a suggestion by Al Petrofsky. The code found below is, with
+; exception of this header and some changes in variable names,
+; entirely written by Al Petrofsky.
 ;
 ; compliance:
 ;   Scheme R5RS (including macros).
@@ -23,40 +26,70 @@
 ;   SE, 27-Feb-2002: revised for 'cut'
 ;   SE, 03-Jun-2002: revised for proc-slot, cute
 ;   SE, 04-Jun-2002: rewritten with internal transformer (no "loop" pattern)
+;   SE, 05-Jun-2002: replace my code by Al's; substituted "constant" etc.
+;     to match the convention in the SRFI-document
 
-; (srfi-26-internal mode params args . cs)
+; (srfi-26-internal-cut slot-names combination . se)
 ;   transformer used internally
-;     mode   : either "cut" or "cute"
-;     params : list of parameters of the specialized procedure
-;     args   : procedure being specialized, followed by its arguments
-;     cs     : consts-or-slots, the qualifiers of the macro
+;     slot-names  : the internal names of the slots
+;     combination : procedure being specialized, followed by its arguments
+;     se          : slots-or-exprs, the qualifiers of the macro
 
-(define-syntax srfi-26-internal
+(define-syntax srfi-26-internal-cut
   (syntax-rules (<> <...>)
 
-    ; construct variable- or fixed-arity procedure
-    ((srfi-26-internal mode (param ...) (proc arg ...) <...>)
-     (lambda (param ... . rest-slot) (apply proc arg ... rest-slot)))
-    ((srfi-26-internal mode (param ...) (proc arg ...))
-     (lambda (param ...) (proc arg ...)))
+    ;; construct fixed- or variable-arity procedure:
+    ;;   (begin proc) throws an error if proc is not an <expression>
+    ((srfi-26-internal-cut (slot-name ...) (proc arg ...))
+     (lambda (slot-name ...) ((begin proc) arg ...)))
+    ((srfi-26-internal-cut (slot-name ...) (proc arg ...) <...>)
+     (lambda (slot-name ... . rest-slot) (apply proc arg ... rest-slot)))
 
-    ; loop over cs
-    ((srfi-26-internal mode (param ...) (proc-arg ...) <> . cs)
-     (srfi-26-internal mode (param ... slot) (proc-arg ... slot) . cs))
-    ((srfi-26-internal "cute" (param ...) (proc-arg ...) (c-op . c-args) . cs)
-     (let ((const (c-op . c-args)))
-       (srfi-26-internal "cute" (param ...) (proc-arg ... const) . cs)))
-    ((srfi-26-internal mode (param ...) (proc-arg ...) const . cs)
-     (srfi-26-internal mode (param ...) (proc-arg ... const) . cs))))
+    ;; process one slot-or-expr
+    ((srfi-26-internal-cut (slot-name ...)   (position ...)      <>  . se)
+     (srfi-26-internal-cut (slot-name ... x) (position ... x)        . se))
+    ((srfi-26-internal-cut (slot-name ...)   (position ...)      nse . se)
+     (srfi-26-internal-cut (slot-name ...)   (position ... nse)      . se))))
+
+; (srfi-26-internal-cute slot-names nse-bindings combination . se)
+;   transformer used internally
+;     slot-names     : the internal names of the slots
+;     nse-bindings   : let-style bindings for the non-slot expressions.
+;     combination    : procedure being specialized, followed by its arguments
+;     se             : slots-or-exprs, the qualifiers of the macro
+
+(define-syntax srfi-26-internal-cute
+  (syntax-rules (<> <...>)
+
+    ;; If there are no slot-or-exprs to process, then:
+    ;; construct a fixed-arity procedure,
+    ((srfi-26-internal-cute
+      (slot-name ...) nse-bindings (proc arg ...))
+     (let nse-bindings (lambda (slot-name ...) (proc arg ...))))
+    ;; or a variable-arity procedure
+    ((srfi-26-internal-cute
+      (slot-name ...) nse-bindings (proc arg ...) <...>)
+     (let nse-bindings (lambda (slot-name ... . x) (apply proc arg ... x))))
+
+    ;; otherwise, process one slot:
+    ((srfi-26-internal-cute
+      (slot-name ...)         nse-bindings  (position ...)   <>  . se)
+     (srfi-26-internal-cute
+      (slot-name ... x)       nse-bindings  (position ... x)     . se))
+    ;; or one non-slot expression
+    ((srfi-26-internal-cute
+      slot-names              nse-bindings  (position ...)   nse . se)
+     (srfi-26-internal-cute
+      slot-names ((x nse) . nse-bindings) (position ... x)       . se))))
 
 ; exported syntax
 
 (define-syntax cut
-  (syntax-rules (<> <...>)
-    ((cut . consts-or-slots)
-     (srfi-26-internal "cut" () () . consts-or-slots))))
+  (syntax-rules ()
+    ((cut . slots-or-exprs)
+     (srfi-26-internal-cut () () . slots-or-exprs))))
 
 (define-syntax cute
-  (syntax-rules (<> <...>)
-    ((cute . consts-or-slots)
-     (srfi-26-internal "cute" () () . consts-or-slots))))
+  (syntax-rules ()
+    ((cute . slots-or-exprs)
+     (srfi-26-internal-cute () () () . slots-or-exprs))))
