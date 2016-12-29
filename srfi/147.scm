4beed76266e0c696391e7d3785b47abd85bf6abe
Marc Nieper-Wißkirchen <marc@nieper-wisskirchen.de>
2016-12-29 18:03:59 +0100

    fixed scoping bug

--- srfi/147.scm
+++ srfi/147.scm
@@ -80,7 +80,7 @@
 (scheme-define-syntax letrec-syntax-aux
   (scheme-syntax-rules ()
     ((letrec-syntax-aux (keyword ...) () (transformer-spec ...) body*)
-     (begin
+     (let ()
        (define-syntax keyword transformer-spec)
        ...
        (let () . body*)))
